5c6436e814d939394cbae4e3efbc6a63939e4c7a
Manish Sahani <friday@Manishs-MacBook-Pro.local>
2019-09-07 16:08:40 +0530

    Frontend Scafoldings removed

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/', function () {
+        return 12;
+    });
+});
