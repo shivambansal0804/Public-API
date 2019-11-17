b1e68858158d765bbb3875cf3148e8f13a66d8c4
Manish Sahani <manishsahani1351111@gmail.com>
2019-11-17 21:05:26 +0530

    Home Api single added

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,7 +14,7 @@ class PageController extends Controller
      */
     public function index()
     {
-        return Story::whereStatus('published')->orderBy('views')->first(10);
+        return Story::whereStatus('published')->orderBy('views')->take(10)->get();
     }
 
     /**
