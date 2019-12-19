87ec5dca12bf929da874489df16224b54b11a18b
Manish Sahani <friday@Manishs-MacBook-Pro.local>
2019-12-19 22:27:33 +0530

    Min

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -49,3 +49,4 @@ class StoryController extends Controller
 
 
 
+
--- app/Http/Controllers/galleryController.php
+++ app/Http/Controllers/galleryController.php
@@ -19,24 +19,24 @@ class GalleryController extends Controller
         $albums = Album::where('status', 'published')->where('album_id', NULL)->get();
         $tempArr = [];
 
-        foreach ($albums as $item){
+        foreach ($albums as $item) {
 
-            array_push($tempArr,
-            ['name' => $item->name,
-            
-            'biliner' => $item->biliner,
-            'slug' => $item->slug,
-            'album_id'=> $item->album_id,
-            'status' => $item->status,
-            'album_imgUrl'=> $item->getFirstMediaUrl('covers', 'cover'),
-            
-            ]);
-           
+            array_push(
+                $tempArr,
+                [
+                    'name' => $item->name,
+
+                    'biliner' => $item->biliner,
+                    'slug' => $item->slug,
+                    'album_id' => $item->album_id,
+                    'status' => $item->status,
+                    'album_imgUrl' => $item->getFirstMediaUrl('covers', 'cover'),
+
+                ]
+            );
         }
 
         return $tempArr;
-        
-
     }
     /**
      * Display the specified resource.
@@ -50,43 +50,45 @@ class GalleryController extends Controller
         $images = $album->image()->where('status', '!=', 'draft')->get();
         $subs = $album->child()->with('user')->get();
 
-        $image_urls=[];
+        $image_urls = [];
         $sub_urls = [];
-        foreach($images as $url){
-            array_push($image_urls,
+        foreach ($images as $url) {
+            array_push(
+                $image_urls,
 
-            ['url' =>  $url->getFirstMediaUrl('images', 'fullscreen'),
-            'name'=> $url->name,
-            'biliner'=> $url->biliner,
-            
-            ]
-        
-        );
-            
-    }
-    foreach ($subs as $subItem){
-            array_push($sub_urls,
-            [
-                'url' => $subItem->getFirstMediaUrl('covers','thumb'),
-                'name'=> $subItem->name,
-                'slug' => $subItem->slug,
-            ]
+                [
+                    'url' =>  $url->getFirstMediaUrl('images', 'fullscreen'),
+                    'name' => $url->name,
+                    'biliner' => $url->biliner,
+
+                ]
+
             );
+        }
+        foreach ($subs as $subItem) {
+            array_push(
+                $sub_urls,
+                [
+                    'album_imgUrl' => $subItem->getFirstMediaUrl('covers', 'thumb'),
+                    'name' => $subItem->name,
+                    'slug' => $subItem->slug,
+                ]
+            );
+        }
 
-    }
 
+        $album_Object =
+            [
+                'name' => $album->name,
+                'biliner' => $album->biliner,
+                'slug' => $album->slug,
+                'album_id' => $album->album_id,
+                'status' => $album->status,
+                'album_imgUrl' => $album->getFirstMediaUrl('covers', 'cover'),
+                'image_info' => $image_urls,
+                'subs_info' => $sub_urls,
 
-        $album_Object = 
-        [   'name' => $album->name,
-            'biliner' => $album->biliner,
-            'slug' => $album->slug,
-            'album_id'=> $album->album_id,
-            'status' => $album->status,
-            'album_imgUrl'=> $album->getFirstMediaUrl('covers', 'cover'),
-            'image_info'=> $image_urls,
-            'subs_info'=> $sub_urls,
-            
-    ];
+            ];
 
         return $album_Object;
     }
