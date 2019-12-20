06f93803af3d797cc16070332102b0893d758f85
Manish Sahani <friday@Manishs-MacBook-Pro.local>
2019-12-20 15:10:51 +0530

    min

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -31,7 +31,7 @@ class CategoryController extends Controller
         $category_object =  Category::where('id', $id)->with('story')->firstOrFail();
         $story_list = $category_object['story'];
         foreach($story_list as $story){
-            $item = Story::whereId($story->id)->first();
+            $item = Story::whereId($story->id)->exclude(['body'])->first();
             $item['imgUrl'] = $item->getFirstMediaUrl('blog_images', 'fullscreen');
             array_push($list,$item);
         }
